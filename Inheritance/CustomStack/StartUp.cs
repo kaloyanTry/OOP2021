ccdc3214858407d77dcd03fda7402ebeb41f9bcf
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-24 15:29:45 +0200

    Add files via upload

---
+++ Inheritance/CustomStack/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackStrings = new StackOfStrings();
+
+            List<string> strings = new List<string>();
+
+            strings.Add("abc");
+            strings.Add("def");
+            strings.Add("xyz");
+            strings.Add("qwerty");
+            strings.Add("123");
+            strings.Add("0987");
+
+            Stack<string> result = stackStrings.AddRange(strings);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
