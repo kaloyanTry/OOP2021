5e5fcbe5f323187c74c377877fab391284a156d2
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-24 15:41:32 +0200

    Add files via upload

---
+++ Inheritance/CustomStack.05/StackOfStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>    {
+
+        public StackOfStrings()
+        {
+
+        }
+
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            foreach (string element in collection)
+            {
+                Push(element);
+            }
+
+            return this;
+        }
+    }
+}
