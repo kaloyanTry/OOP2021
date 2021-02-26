60630060ecdd4e583f2342f6854a784fac1eb76b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-26 16:39:37 +0200

    Add files via upload

---
+++ Inheritance/Exercise/Animals/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public virtual string ProduceSound ()
+        {
+            return string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return this.GetType().Name +
+                Environment.NewLine +
+                $"{Name} {Age} {Gender}";
+        }
+    }
+}
