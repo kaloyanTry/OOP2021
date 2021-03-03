de1bd5d79456c12215097a6ab2193d01d1f7f62c
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-03 07:52:29 +0200

    Add files via upload

---
+++ AbstractionInterfaces/Lab/Shapes/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height 
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+        public int Width 
+        { get { return this.width; }
+            set { this.width = value; }
+        }
+        public void Draw()
+        {
+            for (int row = 0; row < Height; row++)
+            {
+                for (int col = 0; col < Width; col++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
