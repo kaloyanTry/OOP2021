989c0dc17b89db09f0dfa9499788f033b01ced44
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-04 18:39:06 +0200

    Add files via upload

---
+++ AbstractionInterfaces/Exercises/MultipleImplementation/Citizen.cs
@@ -0,0 +1,20 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthday)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthday;
+
+        }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public string Id { get; private set; }
+        public string Birthdate { get; private set; }
+        
+    }
+}
