3c6306840bd8d5af6319bc23c982ef7100090d9f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-05 22:06:19 +0200

    Add files via upload

---
+++ Polimorphism/Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+        public string Name { get; protected set; }
+        public string FavouriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my favourite food is {FavouriteFood}";
+        }
+    }
+}
