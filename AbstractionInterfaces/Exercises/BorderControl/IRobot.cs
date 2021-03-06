a2309a26198c3b1d46c970cbcfd07ac4a587690c
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-06 09:13:42 +0200

    Add files via upload

---
+++ AbstractionInterfaces/Exercises/BorderControl/IRobot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    interface IRobot : IIdentifiable
+    {
+        string Model { get; }
+    }
+}
---
+++ AbstractionInterfaces/Exercises/BorderControl/Robot.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonInfo
+{
+    class Robot : IRobot, IIdentifiable
+    {
+        public Robot(string id, string model)
+        {
+            Id = id;
+            Model = model;
+        }
+        
+        public string Model { get; private set; }
+
+        public string Id { get; private set; }
+    }
+}
