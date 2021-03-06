c3131c3cf43bf537593a11706a6380ce6028db61
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-06 09:19:20 +0200

    Add files via upload

---
+++ AbstractionInterfaces/Exercises/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,26 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string country, int age)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+        public string Country { get; private set; }
+
+        string IPerson.GetName()
+        {
+            return Name;
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+    }
+}
---
+++ AbstractionInterfaces/Exercises/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            while (data[0] != "End")
+            {
+                Citizen citizen = new Citizen(data[0], data[1], int.Parse(data[2]));
+                IPerson person = citizen;
+                IResident resident = citizen;
+
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+
+                data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
