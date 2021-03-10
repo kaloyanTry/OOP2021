7de0b712bd2e97b4ce330bfc0bc2ef023fe7b38f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-10 21:39:16 +0200

    Add files via upload

---
+++ Exceptions/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EnterNumbers
+{
+    public class EnterNumbers
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ReadNumbers(1, 10);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ReadNumbers(int start, int end)
+        {
+            int previusNumber = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    previusNumber = number;
+                }
+
+                if (number < previusNumber || number < start || number > end)
+                {
+                    throw new ArgumentOutOfRangeException("Number not in range");
+                }
+            }
+        }
+    }
+}
