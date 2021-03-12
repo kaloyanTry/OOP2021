9393f4b85e1e40130212d5a382ea0da93d90fe0a
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-12 16:11:17 +0200

    Add files via upload

---
+++ Exceptions/FixingVol2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FixingVol2
+{
+    class FixingVol2
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine($"{num1} x {num2} = {result}");
+                Console.ReadLine();
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
