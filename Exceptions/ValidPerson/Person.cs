2d64d27a012586288fb3901a0bc83f658f19086e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-12 16:43:56 +0200

    Add files via upload

---
+++ Exceptions/ValidPerson/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidPerson
+{
+    public class Person
+    {
+
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName 
+        { 
+            get { return firstName; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The first name cannot be null or empty.");
+                }
+                firstName = value;
+            }     
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The last name cannot be null or empty.");
+                }
+                firstName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age should be in the range [0 ... 120].");
+                }
+                age = value;
+            }
+        }
+    }
+}
---
+++ Exceptions/ValidPerson/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ValidPerson
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person person = new Person(string.Empty, "Goshev", 31);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception throw: {ex.Message}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Exception throw: {ex.Message}");
+            }
+
+            try
+            {
+                Person person = new Person("Stoyan", "Kolev", -1);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception throw: {ex.Message}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Exception throw: {ex.Message}");
+            }
+        }
+    }
+}
