1f3a567034471eb70e0eecffda26fd7c2af6a265
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-12 16:16:14 +0200

    Add files via upload

---
+++ Exceptions/ConvertToDouble.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConvertToDouble
+{
+    class ConvertToDouble
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                double result = Convert.ToDouble(input);
+                Console.WriteLine(result);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                throw ex;
+            }
+        }
+    }
+}
