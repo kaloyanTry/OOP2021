af28eebfeb841d87e7786e7f2be57a29c6b13bbf
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-14 08:37:57 +0200

    Add files via upload

---
+++ Solid/Lab/P01.Stream_Progress/StreamProgressInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public class StreamProgressInfo
+    {
+        //before: private File file;
+        // If we want to stream a music file, we can't
+
+        private IStreamable streamable;
+        public StreamProgressInfo(IStreamable streamable)
+        {
+            this.streamable = streamable;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.streamable.BytesSent * 100) / this.streamable.Length;
+        }
+    }
+}
