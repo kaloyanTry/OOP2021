cd91c12f1fc59a08deb865226a4c3c3551cf6c95
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-17 15:35:03 +0200

    Add files via upload

---
+++ Reflection/Lab/Collector/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string CollectGettersSetters(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+             return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Reflection/Lab/Collector/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersSetters("Stealer.Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}
