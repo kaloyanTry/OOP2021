7b6314d1ae8c6f1d226323308994ec63b5ae498b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-17 15:26:00 +0200

    Add files via upload

---
+++ Reflection/Lab/MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string RevealPrivateMethods(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"All Private Methods of Class: {investigatedClass}");
+            sb.AppendLine($"Base Class: {classType.BaseType.Name}");
+
+            foreach (MethodInfo method in classMethods)
+            {
+                sb.AppendLine(method.Name);
+            }
+
+             return sb.ToString().Trim();
+        }
+    }
+}
