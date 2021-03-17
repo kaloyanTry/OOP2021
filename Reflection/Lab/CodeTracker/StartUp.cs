81a765315566f7660a1bba3a6a32fe398e86d7ca
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-17 16:00:24 +0200

    Add files via upload

---
+++ Reflection/Lab/CodeTracker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [AuthorAttribute("Kaloyan")]
+    public class StartUp
+    {
+        [AuthorAttribute("Georgi")]
+        static void Main(string[] args)
+        {
+            var tracer = new Tracker();
+            tracer.PrintMethodsByAuthor();
+        }
+    }
+}
