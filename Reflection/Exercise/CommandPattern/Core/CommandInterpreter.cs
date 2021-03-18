822a1bac57282302042c0e06de98208f3b3cdb74
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-18 21:23:54 +0200

    Add files via upload

---
+++ Reflection/Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,32 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandArgs = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string commandName = (commandArgs[0] + "Command").ToLower();
+
+            Type commandType = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(n => n.Name.ToLower() == commandName);
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Invalid command.");
+            }
+
+            ICommand instance = Activator.CreateInstance(commandType) as ICommand;
+            if (instance == null)
+            {
+                throw new ArgumentException("Invalid command.");
+            }
+
+            string result = instance.Execute(commandArgs);
+            return result;
+        }
+    }
+}
---
+++ Reflection/Exercise/CommandPattern/Core/Commands/HelloCommand.cs
@@ -0,0 +1,13 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Commands
+{
+    class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            string name = args[1];
+            return $"Hello, {name}";
+        }
+    }
+}
