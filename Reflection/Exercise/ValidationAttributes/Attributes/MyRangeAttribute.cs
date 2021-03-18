eaf6ae356b227973bbf733d51659d2cd5e7dd8eb
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-18 21:22:46 +0200

    Add files via upload

---
+++ Reflection/Exercise/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ValidationAttributes.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minValue;
+        private int maxValue;
+
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+
+        public override bool IsValid(object obj)
+        {
+            if (obj is int valueAsInt)
+            {
+                if (valueAsInt >= minValue && valueAsInt <= maxValue)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            throw new ArgumentException("Invalid type!");
+        }
+    }
+}
---
+++ Reflection/Exercise/ValidationAttributes/Person.cs
@@ -0,0 +1,19 @@
+﻿using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes
+{
+    public class Person
+    {
+        public Person(string fullName, int age)
+        {
+            FullName = fullName;
+            Age = age;
+        }
+
+        [MyRequired]
+        public string FullName { get; set; }
+
+        [MyRange(12, 95)]
+        public int Age { get; set; }
+    }
+}
