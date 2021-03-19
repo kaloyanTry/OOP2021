1126c677e4c1baf1fdba3ff8d926cbe1ea67c783
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-19 19:12:15 +0200

    Add files via upload

---
+++ Encapsulation/Exercise/FootbalTeamGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FootbalTeamGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
