81d844a5cf6fca91be4a62ddedce912a4eac6868
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-19 21:56:38 +0200

    Add files via upload

---
+++ UnitTesting/Lab/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        private decimal amount;
+
+        public BankAccount(decimal amount)
+        {
+            Amount = amount;
+        }
+        public decimal Amount 
+        { 
+            get => amount; 
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("No negatives.");
+                }
+                amount = value;
+            } 
+        }
+    }
+}
---
+++ UnitTesting/Lab/BankAccountTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Bank.Tests
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        [Test]
+        public void WhenAccountInitialiedWithPossitiveValue_AmountChanged()
+        {
+            decimal amount = 5;
+
+            BankAccount bankAccount = new BankAccount(amount);
+
+            Assert.That(bankAccount.Amount, Is.EqualTo(amount));
+            Assert.AreEqual(bankAccount.Amount, amount);
+            Assert.IsTrue(bankAccount.Amount == amount);
+        }
+    }
+}
