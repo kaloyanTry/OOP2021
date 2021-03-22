c5a01e3364307dea65c692e7635414d2d44b413f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-22 07:35:39 +0200

    Add files via upload

---
+++ Exceptions/SquareRoot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExceptionSquareRoot
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                double result = Sqrt(n);
+
+                Console.WriteLine($"Square root of {n} is = {result}");
+
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.Error.WriteLine("Error: " + ex.Message);
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Good-bye");
+            }
+        }
+
+        public static double Sqrt(double value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentOutOfRangeException(nameof(value), "Sqrt for negative numbers is undefined!");
+            }
+            return Math.Sqrt(value);
+        }
+    }
+}
