a701c4ca5c832df5c736443e3c04bf3511c13c32
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-27 17:45:12 +0200

    Add files via upload

---
+++ OldExamsOOP/2020.12.12.examOOP/Task1.Bakerey/Models/BakedFoods/BakedFood.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Bakery.Models.BakedFoods
+{
+    using Bakery.Models.BakedFoods.Contracts;
+
+    public abstract class BakedFood : IBakedFood
+    {
+        private string name;
+        private int portion;
+        private decimal price;
+
+        public BakedFood(string name, int portion, decimal price)
+        {
+            Name = name;
+            Portion = portion;
+            Price = price;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or white space!");
+                }
+                name = value;
+            }
+        }
+
+
+        public int Portion
+        {
+            get => portion;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Portion cannot be less or equal to zero");
+                }
+                portion = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get => price;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price cannot be less or equal to zero!");
+                }
+                price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}: {Portion}g - {Price:F2}";
+        }
+    }
+}
---
+++ OldExamsOOP/2020.12.12.examOOP/Task1.Bakerey/Models/Drinks/Drink.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Bakery.Models.Drinks
+{
+    using Bakery.Models.Drinks.Contracts;
+
+    public abstract class Drink : IDrink
+    {
+        private string name;
+        private int portion;
+        private decimal price;
+        private string brand;
+
+        protected Drink(string name, int portion, decimal price, string brand)
+        {
+            Name = name;
+            Portion = portion;
+            Price = price;
+            Brand = brand;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or white space!");
+                }
+                name = value;
+            }
+        }
+
+        public int Portion
+        {
+            get => portion;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Portion cannot be less or equal to zero");
+
+                }
+                portion = value;
+            }
+        }
+
+
+        public decimal Price
+        {
+            get => price;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price cannot be less or equal to zero!");
+                }
+                price = value;
+            }
+        }
+
+        public string Brand
+        {
+            get => brand;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Brand cannot be null or white space!");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} {Brand} - {Portion}ml - {Price:F2}lv"; 
+        }
+    }
+}
