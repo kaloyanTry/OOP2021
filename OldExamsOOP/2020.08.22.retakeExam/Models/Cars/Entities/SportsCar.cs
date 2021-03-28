069e66679e7f8059c532f4a2e1535aa4ed90b408
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-28 19:14:01 +0300

    Add files via upload

---
+++ OldExamsOOP/2020.08.22.retakeExam/Models/Cars/Entities/SportsCar.cs
@@ -0,0 +1,14 @@
+﻿namespace EasterRaces.Models.Cars.Entities
+{
+    public class SportsCar : Car
+    {
+        private const int InitialCubicCentimeters = 3000;
+        private const int InitialHorsePowerMin = 250;
+        private const int InitialHorsePowerMax = 450;
+
+        public SportsCar(string model, int horsePower) 
+            : base(model, horsePower, InitialCubicCentimeters, InitialHorsePowerMin, InitialHorsePowerMax)
+        {
+        }
+    }
+}
