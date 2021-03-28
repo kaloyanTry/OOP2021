546a8c6618f5f6b2888e58790dd83988f8722645
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-28 08:20:14 +0300

    Add files via upload

---
+++ OldExamsOOP/2020.12.12.examOOP/Task2.Bakerey/Models/Tables/Table.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Bakery.Models.Tables
+{
+    using Bakery.Models.BakedFoods.Contracts;
+    using Bakery.Models.Drinks.Contracts;
+    using Bakery.Models.Tables.Contracts;
+    using Bakery.Utilities.Messages;
+
+    public abstract class Table : ITable
+    {
+        private readonly List<IBakedFood> foodOrders;
+        private readonly List<IDrink> drinkOrders;
+
+        private int capacity;
+        private int numberOfPeople;
+
+        protected Table(int tableNumber, int capacity, decimal pricePerPerson)
+        {
+            TableNumber = tableNumber;
+            Capacity = capacity;
+            PricePerPerson = pricePerPerson;
+            IsReserved = false;
+            foodOrders = new List<IBakedFood>();
+            drinkOrders = new List<IDrink>();
+        }
+
+        public int TableNumber { get; }
+
+        public int Capacity
+        {
+            get => capacity;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidTableCapacity);
+                }
+                capacity = value;
+            }
+        }
+
+        public int NumberOfPeople
+        {
+            get => numberOfPeople;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidNumberOfPeople);
+                }
+                numberOfPeople = value;
+            }
+        }
+
+        public decimal PricePerPerson { get; }
+
+        public bool IsReserved { get; private set; }
+
+
+        public decimal Price
+            => foodOrders.Select(f => f.Price).Sum()
+            + drinkOrders.Select(f => f.Price).Sum()
+            + NumberOfPeople * PricePerPerson;
+
+        public void Clear()
+        {
+            foodOrders.Clear();
+            drinkOrders.Clear();
+            IsReserved = false;
+            Capacity = 0;
+        }
+
+        public decimal GetBill()
+        {
+            return Price;
+        }
+
+        public string GetFreeTableInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Table: {TableNumber}");
+            sb.AppendLine($"Type: {GetType().Name}");
+            sb.AppendLine($"Capacity: {Capacity}");
+            sb.AppendLine($"Price per Person: {PricePerPerson}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void OrderDrink(IDrink drink)
+        {
+            drinkOrders.Add(drink);
+        }
+
+        public void OrderFood(IBakedFood food)
+        {
+            foodOrders.Add(food);
+        }
+
+        public void Reserve(int numberOfPeople)
+        {
+            IsReserved = true;
+            NumberOfPeople = numberOfPeople;
+        }
+    }
+}
