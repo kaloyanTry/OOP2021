091c279c8bb54bb4b6508487316df12f61b8d92c
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-03-28 19:11:45 +0300

    Add files via upload

---
+++ OldExamsOOP/22.08.2020.retakeExam/Models/Races/Entities/Race.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Races.Entities
+{
+    using EasterRaces.Models.Drivers.Contracts;
+    using EasterRaces.Models.Races.Contracts;
+    using EasterRaces.Utilities.Messages;
+    using System.Linq;
+
+    public class Race : IRace
+    {
+        private string name;
+        private int laps;
+        private readonly List<IDriver> drivers;
+
+        public Race(string name, int laps)
+        {
+            Name = name;
+            Laps = laps;
+            drivers = new List<IDriver>();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidName);
+                }
+                name = value;
+            }
+        }
+
+        public int Laps
+        {
+            get => laps;
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidNumberOfLaps);
+                }
+                laps = value;
+            }
+        }
+
+        public IReadOnlyCollection<IDriver> Drivers => drivers;
+
+        public void AddDriver(IDriver driver)
+        {
+            if (driver == null)
+            {
+                throw new ArgumentNullException(ExceptionMessages.DriverInvalid);
+            }
+            else if (!driver.CanParticipate)
+            {
+                throw new ArgumentException(ExceptionMessages.DriverNotParticipate);
+            }
+            else if (drivers.Any(d => d.Name == driver.Name))
+            {
+                throw new ArgumentNullException(ExceptionMessages.DriversExists);
+            }
+
+            drivers.Add(driver);
+        }
+    }
+}
