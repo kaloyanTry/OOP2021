7eda891c024d77c7c1e77b63c3f287594a8058e5
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-15 17:34:18 +0300

    Add files via upload

--- OldExamsOOP/2020.12.19.retakeExamOOP/Task3.UnitTesting/StageTests.cs
+++ OldExamsOOP/2020.12.19.retakeExamOOP/Task3.UnitTesting/StageTests.cs
@@ -3,173 +3,141 @@
 // Test ONLY the Stage class. 
 namespace FestivalManager.Tests
 {
-	using NUnit.Framework;
-	using System;
-
-	//using FestivalManager.Entities; //Comment for judge
-	using System.Collections.Generic;
+    using FestivalManager.Entities; //comment for Judge
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
 	using System.Linq;
 
-	[TestFixture]
+    [TestFixture]
 	public class StageTests
-	{
-		[Test]
-		public void Ctor_ShouldInitializeCollectionCorrectly()
-		{
-			List<Song> songs = new List<Song>();
-			List<Performer> performers = new List<Performer>();
+    {
+		private Stage stage;
+		private List<Song> songs;
+		private List<Performer> performers;
 
+		[SetUp]
+		public void SetUp()
+        {
+			stage = new Stage();
+			songs = new List<Song>();
+			performers = new List<Performer>();
+        }
+
+		[Test]
+		public void Ctor_ShouldBeValidStage()
+        {
+			Assert.IsNotNull(stage);
+        }
+		[Test]
+		public void Ctor_ShouldBeValidSongs()
+		{
 			Assert.IsNotNull(songs);
+		}
+		[Test]
+		public void Ctor_ShouldBeValidPerformer()
+		{
 			Assert.IsNotNull(performers);
 		}
 
 		[Test]
-		public void AddPerformerToStage_ShoudThrowExeption_WhenNull()
-        {
-			Stage stage = new Stage();
-			Performer performer = null;
-
-			Assert.Throws<ArgumentNullException>(() =>
-			{
-				stage.AddPerformer(performer);
-			}, "Can not be null!");
-		}
-
-		[Test]
-		public void AddPerformerToStage_ShoudThrowExeption_WhenLess18()
+		public void AddPerformer_ShouldAddCorrectly()
 		{
-			Stage stage = new Stage();
-			Performer performer = new Performer("U", "2", 12);
-
-			Assert.Throws<ArgumentException>(() =>
-			{
-				stage.AddPerformer(performer);
-			}, "You can only add performers that are at least 18.");
-		}
-
-		[Test]
-		public void AddPerformerToStage_ShoudAddCorrectly()
-		{
-			List<Performer> performers = new List<Performer>();
-			Performer performer = new Performer("U", "2", 62);
+			var performer = new Performer("Fil", "Kir", 25);
 			performers.Add(performer);
-
-			Assert.That(performers.Contains(performer), Is.True);
-		}
-
-
-		[Test]
-		public void AddSongToStage_ShoudThrowExeption_WhenNull()
-		{
-			Stage stage = new Stage();
-			Song song = null;
-
-			Assert.Throws<ArgumentNullException>(() =>
-			{
-				stage.AddSong(song);
-			}, "Can not be null!");
+			Assert.That(performers.Contains(performer));
 		}
 
 		[Test]
-		public void AddSong_ShouldThrowException_WhenLes1Minute()
+	    public void AddPerformerStage_ShouldThrowException_WhenUnder18()
+	    {
+			var performer = new Performer("Fil", "Kir", 2);
+
+			Assert.That(() => stage.AddPerformer(performer), Throws.ArgumentException.With.Message.EqualTo("You can only add performers that are at least 18."));
+		}
+
+		[Test]
+		public void AddSong_Correctly()
         {
-			Stage stage = new Stage();
-			Song song = new Song("Here", new TimeSpan(0, 0, 36));
+			var song = new Song("You", new TimeSpan(0, 3, 24));
+			songs.Add(song);
 
-			Assert.Throws<ArgumentException>(() =>
-			{
-				stage.AddSong(song);
-			}, "You can only add songs that are longer than 1 minute.");
+			Assert.That(songs.Contains(song));
         }
 
 		[Test]
-		public void AddSong_ShouldWorkCorrectly()
-        {
-			List<Song> songs = new List<Song>();
-			Song song = new Song("Time", new TimeSpan(0, 6, 36));
-			songs.Add(song);
-
-			Assert.That(songs.Contains(song), Is.True);
-		}
-
-		[Test]
-		public void AddSongToPerformer_ShouldThrowException_WhenNotValidPerformer()
-        {
-			Stage stage = new Stage();
-			Performer performer = null;
-			Song song = new Song("Time", new TimeSpan(0, 6, 36));
-
-			Assert.Throws<ArgumentException>(() =>
-			{
-				stage.AddSongToPerformer(nameof(song), nameof(performer));
-			}, "Can not be null!");
-		}
-
-		[Test]
-		public void AddSongToPerformer_ShouldThrowException_WhenNotValidSong()
+		public void AddSongStage_ShouldThrowException_WhenUnder1()
 		{
-			Stage stage = new Stage();
-			Performer performer = new Performer("U", "2", 48);
-			Song song = null;
+			var song = new Song("You", new TimeSpan(0, 0, 24));
 
-			Assert.Throws<ArgumentException>(() =>
-			{
-				stage.AddSongToPerformer(nameof(song), nameof(performer));
-			}, "Can not be null!");
+			Assert.That(() => stage.AddSong(song), Throws.ArgumentException.With.Message.EqualTo("You can only add songs that are longer than 1 minute."));
 		}
 
 		[Test]
-		public void AddSongToPerformer_ShouldWorkCorrectly()
-		{
-			Stage stage = new Stage();
-			Performer performer = new Performer("U", "2", 48);
-			Song song = new Song("Time", new TimeSpan(0, 6, 36));
+		public void AddSongTOPerformer_ShouldWorkCorrectly()
+        {
+			var song = new Song("You", new TimeSpan(0, 2, 24));
+			var performer = new Performer("Fil", "Kir", 29);
 
 			performer.SongList.Add(song);
 
-			Assert.That(performer.SongList.Contains(song), Is.True);			
+			Assert.That(performer.SongList.Contains(song));
 		}
 
 		[Test]
-		public void Play_ShouldCountSongsCorrectly()
-        {
-			Performer performer = new Performer("U", "2", 48);
-			Song song = new Song("Time", new TimeSpan(0, 6, 36));
-
+		public void AddSongToPerformer_ShouldThrowExcetion()
+		{
+			var song = new Song("You", new TimeSpan(0, 2, 24));
+			var performer = new Performer("Fil", "Kir", 29);
 			performer.SongList.Add(song);
 
-			Assert.AreEqual(performer.SongList.Count, 1);	
+			Assert.That(performer.SongList.Contains(song));
 		}
 
 		[Test]
-		public void Play_ShouldCountPerformersCorrectly()
+		public void GetPerformer_ShouldCountCorrectlyPerformer()
 		{
-			List<Performer> performers = new List<Performer>();
-			Performer performer = new Performer("U", "2", 48);
+			var performer = new Performer("Fil", "Kir", 29);
+			stage.AddPerformer(performer);
 
+			Assert.That(stage.Performers.Count, Is.EqualTo(1));
+		}
+
+		public void GetPErformer_ShouldReturn()
+		{
+			Assert.Throws<ArgumentException>(() => performers.Contains(new Performer("Aa", "Ka", 144)), "There is no performer with this name."); 
+		}
+
+		[Test]
+		public void AddSongToPerformer_ShouldCountCorrectly()
+		{
+			var song = new Song("You", new TimeSpan(0, 2, 24));
+			var performer = new Performer("Fil", "Kir", 29);
+			performer.SongList.Add(song);
+
+			Assert.That(performer.SongList.Count, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Play_ShouldCountCorrectlyPerformer()
+        {
+			var performer = new Performer("Fil", "Kir", 29);
 			performers.Add(performer);
 
-			Assert.AreEqual(performers.Count, 1);
+			Assert.That(performers.Count, Is.EqualTo(1));
 		}
 
 		[Test]
-		public void GetPerformer_ShouldCountPerformersCorrectly()
-		{
-			List<Performer> performers = new List<Performer>();
-			Performer performer = null;
-			performers.Add(performer);
+		public void ValidateNullSong_ShouldThrowException()
+        {
+			Assert.That(() => stage.AddSong(null), Throws.ArgumentNullException);
+        }
 
-			Assert.AreEqual(performer, null);
-		}
 
 		[Test]
-		public void GetSong_ShouldCountSongsCorrectly()
+		public void ValidateNullPerformer_ShouldThrowException()
 		{
-			List<Song> songs = new List<Song>();
-			Song song = null;
-			songs.Add(song);
-
-			Assert.AreEqual(song, null);
+			Assert.That(() => stage.AddPerformer(null), Throws.ArgumentNullException);
 		}
 	}
 }
