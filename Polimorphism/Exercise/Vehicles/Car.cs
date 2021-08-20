911cee08c6d00a749fbe1224c58acc5c554d0593
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-20 07:26:10 +0300

    Update Car.cs

--- Polimorphism/Exercise/Vehicles/Car.cs
+++ Polimorphism/Exercise/Vehicles/Car.cs
@@ -7,6 +7,7 @@ namespace Vehicles
     public class Car : Vehicle
     {
         private const double CarAirModifier = 0.9;
+        
         public Car(double fuelQuantity, double fuelConsumption) 
             : base(fuelQuantity, fuelConsumption, CarAirModifier)
         {
